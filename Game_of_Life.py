3d35bd97f2b296b02d4aee9f31319f24a2fbdb5d
Aggelos Semoglou <124189083+semoglou@users.noreply.github.com>
2023-03-06 17:59:26 +0200

    Update Game_of_Life.py

--- Game_of_Life.py
+++ Game_of_Life.py
@@ -2,24 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 grid_size = (50, 50) # size of the grid
-grid = np.random.choice([0, 1], size=grid_size, p=[0.5, 0.5]) # initial state of the grid, randomly generated with a 50/50 chance of a cell being alive/dead
+grid = np.random.choice([0, 1], size=grid_size, p=[0.5, 0.5])   # initial state of the grid, randomly generated with a 50/50 chance of a cell being alive/dead
 
-# rules of the game
+# rules of the game & update:
 def update(grid, grid_size):
-    new_grid = np.zeros(grid_size, dtype=int) # create a new empty grid of the same size to update
+    new_grid = np.zeros(grid_size, dtype=int)   # create a new empty grid of the same size to update
     for i in range(grid_size[0]):
         for j in range(grid_size[1]):
-            # Compute the number of live neighbors for each cell
-            num_neighbors = np.sum(grid[max(0, i-1):min(i+2, grid_size[0]), max(0, j-1):min(j+2, grid_size[1])]) - grid[i, j] # count the number of alive neighbors around the cell
+              # Compute the number of live neighbors for each cell
+            num_neighbors = np.sum(grid[max(0, i-1):min(i+2, grid_size[0]), max(0, j-1):min(j+2, grid_size[1])]) - grid[i, j]   # count the number of alive neighbors around the cell
             # Apply the rules of the game
-            if grid[i, j] == 1 and (num_neighbors < 2 or num_neighbors > 3): # rule 1: if a live cell has fewer than 2 or more than 3 live neighbors, it dies
+            if grid[i, j] == 1 and (num_neighbors < 2 or num_neighbors > 3):   # rule 1: if a live cell has fewer than 2 or more than 3 live neighbors, it dies
                 new_grid[i, j] = 0
-            elif grid[i, j] == 0 and num_neighbors == 3: # rule 2: if a dead cell has exactly 3 live neighbors, it becomes alive
+            elif grid[i, j] == 0 and num_neighbors == 3:   # rule 2: if a dead cell has exactly 3 live neighbors, it becomes alive
                 new_grid[i, j] = 1
-            else: # rule 3: otherwise, the cell state stays the same
+            else:   # rule 3: otherwise, the cell state stays the same:
                 new_grid[i, j] = grid[i, j]
-    # Update the grid
-    grid[:] = new_grid[:] # update the old grid with the new one
+      # Update the grid
+    grid[:] = new_grid[:]   # update the old grid with the new one
 
 # Set up the plot
 plt.figure() 
